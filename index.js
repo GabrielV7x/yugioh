eedac211cc2b9fe4434eb30093a832947014b973
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-03-26 15:55:00 +0400

    refactoring done

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 
 
-// console.log(inputs);
-
-positionChoosen = document.querySelector(".choose_btn").addEventListener("click", function(e) {
-  console.log(e.target.innerHTML);
+positionBtn = document.querySelector(".choose_btn").addEventListener("click", ()=> {
 
   const input = document.querySelector(".input_container input[type='radio']:checked")
-  console.log(input);
+  console.log(input.value);
 })

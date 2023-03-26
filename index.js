88cd7d77760d65a14752046ba05f0e169ae46301
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-03-26 18:42:56 +0400

    card info aded to DOM

--- index.js
+++ index.js
@@ -11,13 +11,27 @@ class Card {
 //Creating cards
 const darkMagician = new Card("Dark Magician", 2500, 2100);
 const summonedSkull = new Card("Summoned Skull", 2500, 1200);
-console.log(darkMagician);
+const cardsArray = [darkMagician, summonedSkull]
+
+
+// showing cards in the DOM
+const showCard = (card, index) => {
+// console.log(card.name, card.ATK, card.DEF); ---- TESTING
+let newCard = document.createElement("div")
+newCard.innerHTML =  `${card.name} - ${card.ATK} - ${card.DEF}`
+
+const cardContainer = document.querySelector(".card_container")
+cardContainer.appendChild(newCard)
+}
+
+// calling showCard function on line 19.
+const cardProcessed =  cardsArray.forEach(showCard)
+
 
 
 
 // When the button is clicked, it takes the value of the radio button
 positionBtn = document.querySelector(".choose_btn").addEventListener("click", ()=> {
-
   const input = document.querySelector(".input_container input[type='radio']:checked")
   console.log(input.value);
 })
